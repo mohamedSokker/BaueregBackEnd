c0baf7b7f44e6fe617c65e429d58009050be0436
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-08-20 14:00:07 +0300

    Ready to deploy

--- controllers/filesystem.js
+++ controllers/filesystem.js
@@ -3,24 +3,24 @@ var absPath = process.env.ABS_PATH;
 let abshost = process.env.BASE_URL;
 
 let encodedURL = (string) => {
-  string = string.replace("%", "%25");
-  string = string.replace(" ", "%20");
-  string = string.replace("#", "%23");
-  string = string.replace("&", "%26");
-  string = string.replace('"', "%22");
-  string = string.replace("(", "%28");
-  string = string.replace(")", "%29");
+  string = string.replaceAll("%", "%25");
+  string = string.replaceAll(" ", "%20");
+  string = string.replaceAll("#", "%23");
+  string = string.replaceAll("&", "%26");
+  string = string.replaceAll('"', "%22");
+  string = string.replaceAll("(", "%28");
+  string = string.replaceAll(")", "%29");
   return string;
 };
 
 let decodeURL = (secPath) => {
-  secPath = secPath.replace("%20", " ");
-  secPath = secPath.replace("%23", "#");
-  secPath = secPath.replace("%26", "&");
-  secPath = secPath.replace("%25", "%");
-  secPath = secPath.replace("%22", '"');
-  secPath = secPath.replace("%28", "(");
-  secPath = secPath.replace("%29", ")");
+  secPath = secPath.replaceAll("%20", " ");
+  secPath = secPath.replaceAll("%23", "#");
+  secPath = secPath.replaceAll("%26", "&");
+  secPath = secPath.replaceAll("%25", "%");
+  secPath = secPath.replaceAll("%22", '"');
+  secPath = secPath.replaceAll("%28", "(");
+  secPath = secPath.replaceAll("%29", ")");
   return secPath;
 };
 
