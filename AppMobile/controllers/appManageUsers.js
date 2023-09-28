e6c9ec48a62558eb7fbd86b20c2120febdf95429
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-09-28 22:51:56 +0300

    Ready to deploy

--- AppMobile/controllers/appManageUsers.js
+++ AppMobile/controllers/appManageUsers.js
@@ -90,27 +90,21 @@ const updatemanageUsers = async (req, res) => {
   const getquery =
     "SELECT name FROM sys.columns WHERE object_id = OBJECT_ID('AppMaintUsers')";
   let Results = [];
-  let cond = "";
+  let cond = " WHERE ID = '" + Object.values(req.params)[0] + "'";
   try {
     Results = await getData(getquery);
     Results = Results.recordsets[0];
+    let arrayResult = [];
+    Results.map((result) => {
+      arrayResult.push(result.name);
+      return arrayResult;
+    });
     let keysStatus = true;
     var query = "UPDATE AppMaintUsers SET ";
     const keys = Object.keys(req.body);
-    for (let i = 0; i < Results.length; i++) {
-      if (keys.includes(Results[i]["name"])) {
-        if (Results[i]["name"] == "Password") {
-          query +=
-            Results[i]["name"] +
-            " ='" +
-            (await bcrypt.hash(req.body[Results[i]["name"]], 10)) +
-            "',";
-        } else {
-          query +=
-            Results[i]["name"] + " ='" + req.body[Results[i]["name"]] + "',";
-        }
-      } else if (Results[i]["name"] == "ID") {
-        cond = " WHERE ID = '" + Object.values(req.params)[0] + "'";
+    for (let i = 0; i < keys.length; i++) {
+      if (arrayResult.includes(keys[i]) && keys[i] !== "ID") {
+        query += keys[i] + " ='" + req.body[keys[i]] + "',";
       } else {
         keysStatus = false;
         res.status(404).send("Wrong Arguments");
@@ -119,6 +113,7 @@ const updatemanageUsers = async (req, res) => {
     }
     query = query.slice(0, -1);
     query += cond;
+    console.log(query);
     if (keysStatus === true) {
       const result = await getData(query);
       return res.status(200).json({ success: "true", dataUpdated: result });
--- controllers/Test.js
+++ controllers/Test.js
@@ -4,6 +4,8 @@ const tableDeleteData = require("../Logic/tablesData/tableDeleteData");
 const tableGetAll = require("../Logic/tablesData/tableGetAll");
 const tableGetSingle = require("../Logic/tablesData/tableGetSingle");
 
+const { getData } = require("../functions/getData");
+
 const getAllTest = async (req, res) => {
   try {
     const result = await tableGetAll(`Test`, req.query);
@@ -33,10 +35,46 @@ const addTest = async (req, res) => {
 };
 
 const updateTest = async (req, res) => {
+  // try {
+  //   const targetColVal = Object.values(req.params)[0];
+  //   const result = await tableUpdateData(req.body, `Test`, targetColVal);
+  //   return res.status(200).json({ success: "true", dataUpdated: result });
+  // } catch (error) {
+  //   return res.status(500).json({ message: error.message });
+  // }
+  const getquery =
+    "SELECT name FROM sys.columns WHERE object_id = OBJECT_ID('Test')";
+  let Results = [];
+  let cond = " WHERE ID = '" + Object.values(req.params)[0] + "'";
   try {
-    const targetColVal = Object.values(req.params)[0];
-    const result = await tableUpdateData(req.body, `Test`, targetColVal);
-    return res.status(200).json({ success: "true", dataUpdated: result });
+    Results = await getData(getquery);
+    Results = Results.recordsets[0];
+    let arrayResult = [];
+    Results.map((result) => {
+      arrayResult.push(result.name);
+      return arrayResult;
+    });
+    let keysStatus = true;
+    var query = "UPDATE Test SET ";
+    const keys = Object.keys(req.body);
+    for (let i = 0; i < keys.length; i++) {
+      if (arrayResult.includes(keys[i]) && keys[i] !== "ID") {
+        query += keys[i] + " ='" + req.body[keys[i]] + "',";
+      } else {
+        keysStatus = false;
+        res.status(404).send("Wrong Arguments");
+        break;
+      }
+    }
+    query = query.slice(0, -1);
+    query += cond;
+    console.log(query);
+    if (keysStatus === true) {
+      const result = await getData(query);
+      return res.status(200).json({ success: "true", dataUpdated: result });
+    } else {
+      return res.status(400).json({ message: "Bad request" });
+    }
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
