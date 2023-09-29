89f75564860a385f7114370152b1543551ffcc18
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-09-29 03:08:13 +0300

    Ready to deploy

--- AppMobile/getUserInsite/controllers/logic.js
+++ AppMobile/getUserInsite/controllers/logic.js
@@ -3,7 +3,8 @@ const { getData } = require("../../../functions/getData");
 const logic = async (req, res) => {
   try {
     const fieldsData = req.body;
-    const query = `SELECT Token FROM AppMaintUsers WHERE Location = '${fieldsData.Location}'`;
+    const query = `SELECT Token FROM AppMaintUsers WHERE Location = '${fieldsData.Location}'
+                   AND Role <> 'Operator'`;
     const result = await getData(query);
     return res.status(200).json(result.recordsets[0]);
   } catch (error) {
