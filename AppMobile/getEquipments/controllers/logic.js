15a707c48a353001b5bf7dac727b5f61f77b6254
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-02 12:06:50 +0300

    Ready to deploy

--- AppMobile/getEquipments/controllers/logic.js
+++ AppMobile/getEquipments/controllers/logic.js
@@ -6,15 +6,16 @@ const getEquipments = async (req, res) => {
   let locQuery = ``;
   for (let i = 0; i < bodyData.Location.length; i++) {
     if (i === 0) {
-      locQuery += ` (Location LIKE '%${bodyData.Location[i]}%'`;
+      locQuery += ` (Location = '${bodyData.Location[i]}'`;
     } else if (i === bodyData.Location.length - 1) {
-      locQuery += ` OR Location LIKE '%${bodyData.Location[i]}%')`;
+      locQuery += ` OR Location = '${bodyData.Location[i]}')`;
     } else {
-      locQuery += ` OR Location LIKE '%${bodyData.Location[i]}%'`;
+      locQuery += ` OR Location = '${bodyData.Location[i]}'`;
     }
   }
   const query = `SELECT * FROM Equipments_Location WHERE ${locQuery} 
                     AND End_Date IS NULL AND Equipment_Type = '${bodyData.Equipment_Type}'`;
+  console.log(query);
   try {
     const result = await getData(query);
     return res.status(200).json(result.recordsets[0]);
