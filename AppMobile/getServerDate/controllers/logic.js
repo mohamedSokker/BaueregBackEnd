0507502030e1a8fb57944fd19078be03a4678f25
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-05 01:14:41 +0300

    Ready to deploy

--- AppMobile/getServerDate/controllers/logic.js
+++ AppMobile/getServerDate/controllers/logic.js
@@ -2,7 +2,7 @@ const { getData } = require("../../../functions/getData");
 
 const logic = async (req, res) => {
   try {
-    const query = `SELECT TOP 1 GETDATE() AS Date FROM AppMaintMaintenance`;
+    const query = `SELECT TOP 1 DATEADD(HOUR, 9, GETDATE()) AS Date FROM AppMaintMaintenance`;
     const result = await getData(query);
     return res.status(200).json(result.recordsets[0]);
   } catch (error) {
