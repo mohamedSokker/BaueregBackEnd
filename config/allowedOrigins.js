5de34d2eb47ef281e7e90594c9deffb9ec7aec90
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-16 09:27:40 +0300

    Ready to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://mhsokker.ddnsfree.com:3000"];
+const allowedOrigins = [
+  "http://mhsokker.ddnsfree.com:3000",
+  "https://bauereg.onrender.com",
+];
 
 module.exports = allowedOrigins;
