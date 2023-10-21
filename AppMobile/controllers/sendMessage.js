f991081bbbcd9c12e69a6c810622e4772b9d114e
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-22 02:04:30 +0300

    Ready to deploy

--- AppMobile/controllers/sendMessage.js
+++ AppMobile/controllers/sendMessage.js
@@ -2,6 +2,8 @@ const admin = require("firebase-admin");
 const { initializeApp } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+
 // const serviceAccount = require("path/to/serviceAccountKey.json");
 
 try {
@@ -10,7 +12,7 @@ try {
       type: "service_account",
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRTIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: privateKey,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
