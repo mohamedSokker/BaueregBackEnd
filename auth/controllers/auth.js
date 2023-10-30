9a3e01363cb0e055aeacbc3d1180b8ccc70c1ec8
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-30 10:40:04 +0200

    test auth

--- auth/controllers/auth.js
+++ auth/controllers/auth.js
@@ -21,7 +21,7 @@ let authapp = (endPointName) => {
 
       Results = Results.recordsets[0];
       const roles = JSON.parse(Results[0]["UserRole"]);
-      // console.log(roles);
+      console.log(roles);
 
       if (roles?.Admin) {
         next();
