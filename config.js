902a524116de6c2eaf5767ceafb43fa08ebab476
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-11-02 07:29:20 +0200

    Push Notification

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const config = {
   password: process.env.CONFIG_PASSWORD,
   server: process.env.CONFIG_SERVER,
   database: process.env.CONFIG_DATABASE,
+  port: Number(process.env.CONFIG_PORT),
   synchronize: true,
   trustServerCertificate: true,
 };
