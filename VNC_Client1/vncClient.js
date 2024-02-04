ffa7907fd56133b92a30fb944ad07c7354e65a96
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-02-04 22:59:08 +0200

    VNC-Client 33 Try

--- VNC_Client1/vncClient.js
+++ VNC_Client1/vncClient.js
@@ -8,9 +8,8 @@ const initOptions = {
   encodings: [
     // Encodings sent to server, in order of preference
     VncClient.consts.encodings.copyRect,
-    // VncClient.consts.encodings.zrle,
-    // VncClient.consts.encodings.tight,
-    // VncClient.consts.encodings.hextile,
+    VncClient.consts.encodings.zrle,
+    VncClient.consts.encodings.hextile,
     VncClient.consts.encodings.raw,
     // VncClient.consts.encodings.pseudoDesktopSize,
     // VncClient.consts.encodings.pseudoCursor,
