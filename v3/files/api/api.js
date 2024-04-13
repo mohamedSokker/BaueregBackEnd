056659148322ef448c330be6aa08c15cf48afcf4
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-04-13 19:06:09 +0200

    V3

--- v3/files/api/api.js
+++ v3/files/api/api.js
@@ -83,6 +83,29 @@ const filesEndPoints = (app) => {
     const filePath = path.join(__dirname, "..", `/${decodeURL(inputPath)}`);
     res.sendFile(filePath);
   });
+
+  app.get("/AppGetFiles", (req, res) => {
+    try {
+      let basePath = process.env.BASE_PATH;
+      if (req.query.fullpath.length < basePath.length) {
+        return res.status(403).json({ message: "Unauthorized" });
+      } else {
+        let arrayOfFiles = fs.readdirSync(`${req.query.fullpath}`);
+        let filesList = [];
+
+        arrayOfFiles.forEach((file) => {
+          if (fs.lstatSync(`${req.query.fullpath}/${file}`).isFile()) {
+            filesList.push({ file: file, type: "file" });
+          } else {
+            filesList.push({ file: file, type: "folder" });
+          }
+        });
+        return res.status(200).json({ success: true, data: filesList });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  });
 };
 
 module.exports = { filesEndPoints };
