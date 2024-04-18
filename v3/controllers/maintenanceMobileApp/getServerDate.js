b8f878de19f4638bd73bc6d0f83c466196aa0a2d
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-04-18 17:41:42 +0200

    V3 reduce memory with stream JSONStream get Server Date

--- v3/controllers/maintenanceMobileApp/getServerDate.js
+++ v3/controllers/maintenanceMobileApp/getServerDate.js
@@ -6,7 +6,7 @@ const logic = async (req, res) => {
     // const query = `SELECT TOP 1 DATEADD(HOUR, 9, GETDATE()) AS Date FROM AppMaintMaintenance`;
     // const result = await getData(query);
     // return res.status(200).json(result.recordsets[0]);
-    return res.status(200).json({ Date: new Date().toISOString() });
+    return res.status(200).json([{ Date: new Date().toISOString() }]);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
