8d289ab896cd6e44048c86d28bb17b8d496c691d
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-04-18 17:35:07 +0200

    V3 reduce memory with stream JSONStream get Server Date

--- v3/controllers/maintenanceMobileApp/AppMaintMaintenance.js
+++ v3/controllers/maintenanceMobileApp/AppMaintMaintenance.js
@@ -65,7 +65,7 @@ const getAppMaintMaintenance = async (req, res) => {
 
 const addAppMaintMaintenance = async (req, res) => {
   try {
-    console.log(req, body);
+    console.log(req.body);
     const result = await addData(
       req.body,
       "AppMaintMaintenance",
