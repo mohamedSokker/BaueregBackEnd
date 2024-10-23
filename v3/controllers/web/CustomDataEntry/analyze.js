48940c8e9ed21aa612c9dcbdda2785a4864d86ae
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-10-23 11:34:50 +0300

    Adding All Data instead of only date at custom data entry

--- v3/controllers/web/CustomDataEntry/analyze.js
+++ v3/controllers/web/CustomDataEntry/analyze.js
@@ -21,7 +21,7 @@ const Analyze = async (req, res) => {
 
     const excelData = sheerToJson(workbook.Sheets["Sheet1"]);
 
-    console.log(excelData);
+    // console.log(excelData);
 
     let savedData = {};
 
@@ -72,13 +72,15 @@ const Analyze = async (req, res) => {
               : item[key] &&
                 targetData[0]?.Fields[key]?.canBeEmpty &&
                 item[key] === "Null"
-              ? null
+              ? "Null"
               : item[key]
             : "";
       });
       data.push(newItem);
       dataWithID.push(newItemWithID);
     });
+
+    console.log(dataWithID);
 
     if (dataWithID.length > 0) {
       const validate = await validateData(dataWithID, savedData, targetData);
