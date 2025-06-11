a7bef5a0ac366046601bf9af85ca448c32b657e7
mohamedSokker <mhmd.sokkar92@gmail.com>
2025-06-11 06:38:52 +0300

    Socket Best practice

--- v3/socket/socket.js
+++ v3/socket/socket.js
@@ -1,9 +1,10 @@
 const socketio = require("socket.io");
 
-const {
-  addConndection,
-  client,
-} = require("../services/web/VNC_Client1/vncClient");
+const { setupMaintAppHandlers } = require("./MaintAppHandler");
+const { setupVNCHandlers } = require("./VNCHandler");
+const { setupSparepartsHandlers } = require("./SparepartAppHandler");
+
+const { client } = require("../services/web/VNC_Client1/vncClient");
 const {
   handleDisconnect,
 } = require("../services/web/VNC_Client1/handleConnection");
@@ -24,94 +25,33 @@ const socketFn = (server) => {
     },
   });
 
+  const disconnectHandle = (socket, io) => {
+    delete rooms[socket?.id];
+    delete users[socket?.id];
+    console.log(rooms);
+    if (Object.keys(rooms).length === 0) {
+      console.log(client._connected);
+      portsCreated = [];
+      handleDisconnect(client, io, 8000, []);
+      if (client._connected) client.resetState();
+      console.log("Disconnected", client._connected);
+    }
+  };
+
   io.on("connection", (socket) => {
     console.log(`New Connection ${socket.id}`);
 
-    socket.emit("userID", {
-      id: socket.id,
-      appVersion: 6,
-      sparePartAppVersion: 2,
-    });
-
     socket.emit("RequestUserName", "RequestUserName");
 
-    socket.on("join-message", async (roomId) => {
-      console.log("join-message triggered");
-      socket.join(roomId);
-      rooms = { ...rooms, [socket.id]: roomId };
-      console.log(rooms);
-      const port = Number(roomId);
-      await addConndection(socket, port, portsCreated, io);
-    });
+    setupVNCHandlers(io, socket, rooms, portsCreated);
 
-    // socket.on("requestAvData", async () => {
-    //   await availability(io);
-    // });
+    setupMaintAppHandlers(io, socket, users);
 
-    socket.on("leave-room", (roomId) => {
-      delete rooms[socket?.id];
-      if (Object.keys(rooms).length === 0) {
-        console.log(client._connected);
-        portsCreated = [];
-        handleDisconnect(client, io, 8000, portsCreated);
-        if (client._connected) client.resetState();
-        console.log("Disconnected", client._connected);
-      }
-      socket.leave(roomId);
-    });
-
-    socket.on("type", function (data) {
-      var room = JSON.parse(data).room;
-      socket.broadcast.to(room).emit("type", data);
-    });
-
-    socket.on("userName", (data) => {
-      console.log(`New Connection ${data} => ${socket.id}`);
-      users = { ...users, [socket.id]: data };
-      console.log(users);
-    });
-
-    socket.on("scanned", (data) => {
-      console.log("socket scanned => checkScan");
-      socket.to(data.split("==")[1]).emit("checkScan", data);
-    });
-
-    socket.on("successScan", (data1) => {
-      console.log("socket successScan => confirmScan");
-      socket.to(data1?.data?.split("==")[0]).emit("confirmScan", data1);
-    });
-
-    socket.on("updateAppData", (data) => {
-      console.log("socket updateAppData => appDataUpdate");
-      socket.broadcast.emit("appDataUpdate", data);
-    });
-
-    socket.on("appNewMaint", (data) => {
-      console.log("socket appNewMaint => appNewMessage");
-      socket.broadcast.emit("appNewMessage", data);
-    });
-
-    socket.on("appFinishedMaint", (data) => {
-      socket.broadcast.emit("appFinishedMessage", data);
-    });
-
-    socket.on("TaskEdited", (data) => {
-      console.log(data);
-      socket.broadcast.emit("UpdateTask", data);
-    });
+    setupSparepartsHandlers(io, socket);
 
     socket.on("disconnect", () => {
       console.log(`${users[socket?.id]} Connection Lost`);
-      delete rooms[socket?.id];
-      delete users[socket?.id];
-      console.log(rooms);
-      if (Object.keys(rooms).length === 0) {
-        console.log(client._connected);
-        portsCreated = [];
-        handleDisconnect(client, io, 8000, portsCreated);
-        if (client._connected) client.resetState();
-        console.log("Disconnected", client._connected);
-      }
+      disconnectHandle(socket, io);
     });
   });
 };
